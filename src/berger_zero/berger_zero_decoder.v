a8f5a6fea22a1ed47c5d6ef84e6e60d3b0bc1615
TalhaIsrar <talhaisrar02@gmail.com>
2025-07-26 12:56:07 +0200

    Added Berger B0 basic code

--- src/berger_zero/berger_zero_decoder.v
+++ src/berger_zero/berger_zero_decoder.v
@@ -1,51 +1,21 @@
 module berger_zero_decoder(
     input  [11:0] in_code,
-    output reg [7:0] out_data,
-    output reg error_corrected
+    output [7:0] out_data,
+    output error_detected
 );
 
-    wire [3:0] syndrome;
-    reg  [11:0] corrected_code;
+    wire [3:0] ones_count;
+    wire [3:0] zeros_count;
 
-    // Compute syndrome
-    assign syndrome[3] = in_code[11] ^ in_code[10] ^ in_code[9] ^ in_code[8] ^ in_code[7]; 
-    assign syndrome[2] = in_code[11] ^ in_code[6]  ^ in_code[5] ^ in_code[4] ^ in_code[3];
-    assign syndrome[1] = in_code[10] ^ in_code[9]  ^ in_code[6] ^ in_code[5] ^ in_code[2] ^ in_code[1];
-    assign syndrome[0] = in_code[10] ^ in_code[8]  ^ in_code[6] ^ in_code[4] ^ in_code[2] ^ in_code[0];
+    // in_code[11:4] = original data (8 bits)
+    // in_code[3:0]  = Berger checksum (number of 0s in data)
 
-    always @(*) begin
-        // Default assignment to avoid latch
-        corrected_code = in_code;
-        error_corrected = 0; 
+    assign ones_count = in_code[0] + in_code[1] + in_code[2] + in_code[3] +
+                    in_code[4] + in_code[5] + in_code[6] + in_code[7]; 
 
-        if (syndrome != 4'b0000 && syndrome <= 4'd12) begin
-            case (syndrome)
-                4'd1:  corrected_code[0]  = ~corrected_code[0];
-                4'd2:  corrected_code[1]  = ~corrected_code[1];
-                4'd3:  corrected_code[2]  = ~corrected_code[2];
-                4'd4:  corrected_code[3]  = ~corrected_code[3];
-                4'd5:  corrected_code[4]  = ~corrected_code[4];
-                4'd6:  corrected_code[5]  = ~corrected_code[5];
-                4'd7:  corrected_code[6]  = ~corrected_code[6];
-                4'd8:  corrected_code[7]  = ~corrected_code[7];
-                4'd9:  corrected_code[8]  = ~corrected_code[8];
-                4'd10: corrected_code[9]  = ~corrected_code[9];
-                4'd11: corrected_code[10] = ~corrected_code[10];
-                4'd12: corrected_code[11] = ~corrected_code[11];
-                default: ; // Do nothing
-            endcase
-            error_corrected = 1;
-        end
-
-        // Assign to output data
-        out_data[0] = corrected_code[2];
-        out_data[1] = corrected_code[4];
-        out_data[2] = corrected_code[5];
-        out_data[3] = corrected_code[6];
-        out_data[4] = corrected_code[8];
-        out_data[5] = corrected_code[9];
-        out_data[6] = corrected_code[10];
-        out_data[7] = corrected_code[11];
-    end
+    assign zeros_count = 4'd8 - ones_count;
+    
+    assign error_detected = zeros_count != in_code[3:0];
+    assign out_data = in_code[11:4];
 
 endmodule
--- src/berger_zero/berger_zero_encoder.v
+++ src/berger_zero/berger_zero_encoder.v
@@ -4,22 +4,16 @@ module berger_zero_encoder(
 );
 
     wire [11:0] code;
+    wire [3:0] ones_count;
 
     // Assign the data bits
-    assign code[11] = input_data[7];
-    assign code[10] = input_data[6];
-    assign code[9]  = input_data[5];
-    assign code[8]  = input_data[4];
-    assign code[6]  = input_data[3];
-    assign code[5]  = input_data[2];
-    assign code[4]  = input_data[1];
-    assign code[2]  = input_data[0];
+    assign code [11:4] = input_data;
 
-    // Compute parity bits
-    assign code[8] = code[11] ^ code[10] ^ code[9] ^ code[8]; 
-    assign code[3] = code[11] ^ code[6]  ^ code[5] ^ code[4];
-    assign code[1] = code[10] ^ code[9]  ^ code[6] ^ code[5] ^ code[2];
-    assign code[0] = code[10] ^ code[8]  ^ code[6] ^ code[4] ^ code[2];
+    // Compute sum bits
+    assign ones_count = input_data[0] + input_data[1] + input_data[2] + input_data[3] +
+                        input_data[4] + input_data[5] + input_data[6] + input_data[7]; 
+    
+    assign code [3:0] = 4'd8 - ones_count;
 
     // Assign to output
     assign output_code = code;
