7ffd10cf1b88db41e9fd0a2481033e5c37842134
TalhaIsrar <talhaisrar02@gmail.com>
2025-07-26 19:47:35 +0200

    Fixed bug on Berger B0 decoder and added testbench

--- src/berger_zero/berger_zero_decoder.v
+++ src/berger_zero/berger_zero_decoder.v
@@ -10,8 +10,8 @@ module berger_zero_decoder(
     // in_code[11:4] = original data (8 bits)
     // in_code[3:0]  = Berger checksum (number of 0s in data)
 
-    assign ones_count = in_code[0] + in_code[1] + in_code[2] + in_code[3] +
-                    in_code[4] + in_code[5] + in_code[6] + in_code[7]; 
+    assign ones_count = in_code[4] + in_code[5] + in_code[6]  + in_code[7] +
+                        in_code[8] + in_code[9] + in_code[10] + in_code[11];
 
     assign zeros_count = 4'd8 - ones_count;
     
